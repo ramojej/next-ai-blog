d0caf87f19f1bf895cecf3c9891f43d8f6068e23
ramojej <jejomardorongon@gmail.com>
2023-02-14 21:40:44 +0800

    Added routes

---
+++ pages/post/[postId].js
@@ -0,0 +1,7 @@
+export default function Post() {
+  return (
+    <div>
+      <h1>this is the random post page</h1>
+    </div>
+  )
+}
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenTopup() {
+  return (
+    <div>
+      <h1>this is the tokentopup page</h1>
+    </div>
+  )
+}
